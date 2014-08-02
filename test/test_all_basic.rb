5f5ae9399b69e784684e336c8b0b8001cdfa11b0
Benjamin Fleischer <github@benjaminfleischer.com>
2014-08-01 21:51:29 -0500

    Remove test/unit monkeypatch that test output really noisy

--- test/test_all_basic.rb
+++ test/test_all_basic.rb
@@ -6,20 +6,6 @@ require 'test/unit/ui/console/testrunner'  if !RUBY_VERSION[/^1\.9|^2/]
 puts RUBY_VERSION
 puts RUBY_VERSION.class
 
-module Test
-    module Unit
-        class TestCase
-            alias :_old_run_ :run
-            def run(result, &blk)
-                method_name = RUBY_VERSION[/^1\.9|^2/] ? self.__name__ : @method_name
-                puts "Running #{method_name}"
-                _old_run_(result, &blk)
-            end
-        end
-    end
-end
-
-
 $:.push('.')
 
 IMAGES_DIR = '../doc/ex/images'
