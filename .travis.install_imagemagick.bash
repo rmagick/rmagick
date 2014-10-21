2df6562212a09798ee2a279beffea8bb22c51dc7
Benjamin Fleischer <github@benjaminfleischer.com>
2014-10-21 10:03:38 -0500

    Extract multiline script from travis.yml, as recommended

---
+++ .travis.install_imagemagick.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+  dpkg --list imagemagick
+  sudo apt-get remove imagemagick
+  sudo apt-get install build-essential libx11-dev libxext-dev zlib1g-dev libpng12-dev libjpeg-dev libfreetype6-dev libxml2-dev
+  sudo apt-get build-dep imagemagick
+  wget http://www.imagemagick.org/download/releases/ImageMagick-${IMAGEMAGICK_VERSION}.tar.gz
+  tar -xzvf ImageMagick-${IMAGEMAGICK_VERSION}.tar.gz
+  cd ImageMagick-${IMAGEMAGICK_VERSION}
+  ./configure --prefix=/usr $CONFIGURE_OPTIONS
+  sudo make install
+  cd ..
+  sudo ldconfig
+else
+  brew update
+  brew install imagemagick
+fi
