738fc2b34534a25c2de1685379e3502865f271fd
Ilya Vassilevsky <vassilevsky@gmail.com>
2015-02-16 18:06:07 +0300

    Unattended package installation and removal with apt-get

--- before_install_linux.sh
+++ before_install_linux.sh
@@ -1,7 +1,7 @@
 dpkg --list imagemagick
 sudo apt-get update
-sudo apt-get remove imagemagick
-sudo apt-get install build-essential libx11-dev libxext-dev zlib1g-dev libpng12-dev libjpeg-dev libfreetype6-dev libxml2-dev
+sudo apt-get remove -y imagemagick
+sudo apt-get install -y build-essential libx11-dev libxext-dev zlib1g-dev libpng12-dev libjpeg-dev libfreetype6-dev libxml2-dev
 sudo apt-get build-dep imagemagick
 wget http://www.imagemagick.org/download/releases/ImageMagick-${IMAGEMAGICK_VERSION}.tar.gz
 tar -xzvf ImageMagick-${IMAGEMAGICK_VERSION}.tar.gz
