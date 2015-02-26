5b425877f6ae772c7bd0a960c973ed06072d9c87
Ilya Vassilevsky <vassilevsky@gmail.com>
2015-02-27 00:46:05 +0300

    Remove tabs

--- doc/ex/text_antialias.rb
+++ doc/ex/text_antialias.rb
@@ -9,11 +9,11 @@ gc.fill('black')
 gc.stroke('transparent')
 
 if RUBY_PLATFORM =~ /mswin32/
-	 gc.font_family('Georgia')
-	 gc.pointsize(152)
+  gc.font_family('Georgia')
+  gc.pointsize(152)
 else
-	 gc.font_family('courier')
-	 gc.pointsize(200)
+  gc.font_family('courier')
+  gc.pointsize(200)
 end
 gc.font_weight(Magick::BoldWeight)
 
