40a85115801453741d88ef6170dec036c41b23e0
u338_steven <u338.steven@gmail.com>
2015-06-01 18:51:16 +0900

    `Gem::Version` requires `rubygems` for Ruby 1.8

--- ext/RMagick/extconf.rb
+++ ext/RMagick/extconf.rb
@@ -1,5 +1,6 @@
 lib_dir = File.expand_path('../../lib', File.dirname(__FILE__))
 $LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
+require 'rubygems'
 require 'mkmf'
 require 'date'
 
