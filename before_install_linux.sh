64eda650300fe657e777d4e9140a62930b50d3e1
Johannes Barre <igel@igels.net>
2015-07-23 18:15:42 +0200

    Download Imagemagick as a tar.xz
    The tar.gz files are not available anymore

--- before_install_linux.sh
+++ before_install_linux.sh
@@ -5,13 +5,13 @@ sudo apt-get install -y build-essential libx11-dev libxext-dev zlib1g-dev libpng
 sudo apt-get build-dep -y imagemagick
 case $IMAGEMAGICK_VERSION in
     latest)
-        wget http://www.imagemagick.org/download/ImageMagick.tar.gz
-        tar -xzvf ImageMagick.tar.gz
+        wget http://www.imagemagick.org/download/ImageMagick.tar.xz
+        tar -xf ImageMagick.tar.xz
         cd ImageMagick-*
     ;;
     *)
-        wget http://www.imagemagick.org/download/releases/ImageMagick-${IMAGEMAGICK_VERSION}.tar.gz
-        tar -xzvf ImageMagick-${IMAGEMAGICK_VERSION}.tar.gz
+        wget http://www.imagemagick.org/download/releases/ImageMagick-${IMAGEMAGICK_VERSION}.tar.xz
+        tar -xf ImageMagick-${IMAGEMAGICK_VERSION}.tar.xz
         cd ImageMagick-${IMAGEMAGICK_VERSION}
     ;;
 esac
