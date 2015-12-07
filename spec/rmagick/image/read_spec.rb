c8ad4d96992d98c67cd7875f7c6eef55c94e4791
Alexandr Fedorov <alexandrfedorov@gmail.com>
2015-12-07 04:43:10 +0400

    added tests

---
+++ spec/rmagick/image/read_spec.rb
@@ -0,0 +1,26 @@
+require 'timeout'
+
+RSpec.describe Magick::Image, '#read' do
+  describe 'issue #200' do
+    before do
+      pid = Process.spawn File.join(SUPPORT_DIR, 'issue_200', 'app.rb'), :err => :close, :out => :close
+      begin
+        Timeout::timeout(1) do
+          _, @status = Process.waitpid2 pid
+        end
+      rescue Timeout::Error
+        Process.kill('KILL', pid)
+        _, @status = Process.waitpid2 pid
+      end
+    end
+
+    it 'not hangs with nil argument' do
+      expect(@status.signaled?).to be_falsey
+    end
+
+    it 'raise error with nil argument' do
+      expect(@status.success?).to be_truthy
+      expect{ Magick::Image.read(nil) }.to raise_error(Magick::ImageMagickError, /unable to open image nil/)
+    end
+  end
+end
---
+++ spec/support/issue_200/app.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+require 'rmagick'
+
+Magick::Image.read(nil) rescue nil
