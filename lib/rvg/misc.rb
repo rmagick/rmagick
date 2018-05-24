00ed4b5eae2c692c8698cfbe473c7908e63c2ad5
Pavel Lobashov <shockwavenn@gmail.com>
2018-05-24 15:55:50 +0300

    Fix RuboCop `Lint/EmptyWhen` cop

--- lib/rvg/misc.rb
+++ lib/rvg/misc.rb
@@ -133,8 +133,6 @@ module Magick
           case @ctx.text_attrs.baseline_shift
             when :baseline
               x = 0
-            when :sub
-
             when :super
               x = -x
             when /[-+]?(\d+)%/
@@ -187,8 +185,6 @@ module Magick
 
           # Align the first glyph
           case @ctx.text_attrs.glyph_orientation_horizontal
-            when 0
-
             when 90
               y -= dy
             when 180
