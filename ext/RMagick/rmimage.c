bdf863f990b662a3fa40f5d360290430182b9890
Watson <watson1978@gmail.com>
2018-09-10 05:25:52 +0900

    fix ContrastImage() argument

--- ext/RMagick/rmimage.c
+++ ext/RMagick/rmimage.c
@@ -4041,6 +4041,7 @@ Image_contrast(int argc, VALUE *argv, VALUE self)
 {
     Image *image, *new_image;
     unsigned int sharpen = 0;
+    ExceptionInfo *exception;
 
     image = rm_check_destroyed(self);
     if (argc > 1)
@@ -4054,8 +4055,10 @@ Image_contrast(int argc, VALUE *argv, VALUE self)
 
     new_image = rm_clone_image(image);
 
-    (void) ContrastImage(new_image, sharpen);
-    rm_check_image_exception(new_image, DestroyOnError);
+    exception = AcquireExceptionInfo();
+    (void) ContrastImage(new_image, sharpen, exception);
+    rm_check_exception(exception, new_image, DestroyOnError);
+    (void) DestroyExceptionInfo(exception);
 
     return rm_image_new(new_image);
 }
