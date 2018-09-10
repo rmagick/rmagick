7478ccd93690f8a55e25c5fd10aa1d0a1653dd5f
Watson <watson1978@gmail.com>
2018-09-10 10:31:28 +0900

    fix NormalizeImage() argument

--- ext/RMagick/rmimage.c
+++ ext/RMagick/rmimage.c
@@ -9415,12 +9415,15 @@ VALUE
 Image_normalize(VALUE self)
 {
     Image *image, *new_image;
+    ExceptionInfo *exception;
 
     image = rm_check_destroyed(self);
     new_image = rm_clone_image(image);
 
-    (void) NormalizeImage(new_image);
-    rm_check_image_exception(new_image, DestroyOnError);
+    exception = AcquireExceptionInfo();
+    (void) NormalizeImage(new_image, exception);
+    rm_check_exception(exception, new_image, DestroyOnError);
+    (void) DestroyExceptionInfo(exception);
 
     return rm_image_new(new_image);
 }
