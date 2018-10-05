d18ef2841b4a148f1a7ab593b2468bb1512e2115
Adit Mehta <aditmehta9@gmail.com>
2018-10-05 19:11:38 +0530

    Added RSpec tests for delay_pre function

---
+++ spec/rmagick/image/delay_pre_spec.rb
@@ -0,0 +1,14 @@
+RSpec.describe Magick::ImageList, '#delay_pre' do
+
+  let(:imgList) {
+  	Magick::ImageList.new(IMAGES_DIR+'/Hot_Air_Balloons.jpg', IMAGES_DIR+'/Hot_Air_Balloons_H.jpg')
+  }
+
+  it 'returns a gif image' do
+    res = imgList.delay_pre
+    expect(res).to be_instance_of(Magick::Image)
+    expect(res).not_to eq img1
+    expect(res.format).to eq 'GIF'
+  end
+
+end
