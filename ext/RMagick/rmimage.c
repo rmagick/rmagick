76f2fdb429ba438c7f1ba66978db291b4cacd904
Watson <watson1978@gmail.com>
2018-10-08 16:09:55 +0900

    BilevelImageChannel() -> SetImageChannelMask() + BilevelImage()

--- ext/RMagick/rmimage.c
+++ ext/RMagick/rmimage.c
@@ -1202,6 +1202,7 @@ Image_bilevel_channel(int argc, VALUE *argv, VALUE self)
     Image *image, *new_image;
     ChannelType channels;
     double threshold;
+    ExceptionInfo *exception;
 
     image = rm_check_destroyed(self);
     channels = extract_channels(&argc, argv);
@@ -1218,8 +1219,11 @@ Image_bilevel_channel(int argc, VALUE *argv, VALUE self)
     threshold = NUM2DBL(argv[0]);
     new_image = rm_clone_image(image);
 
-    (void)BilevelImageChannel(new_image, channels, threshold);
-    rm_check_image_exception(new_image, DestroyOnError);
+    exception = AcquireExceptionInfo();
+    SetImageChannelMask(new_image, channels);
+    (void)BilevelImage(new_image, threshold, exception);
+    rm_check_exception(exception, new_image, DestroyOnError);
+    (void) DestroyExceptionInfo(exception);
 
     return rm_image_new(new_image);
 }
