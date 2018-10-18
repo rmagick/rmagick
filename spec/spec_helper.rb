cbe8f3ae1ca5d0e4a94b46448f18169668524590
Watson <watson1978@gmail.com>
2018-10-19 00:07:21 +0900

    skip channel_entropy spec with unsupported ImageMagick
    GetImageChannelEntropy() API was implemented in ImageMagick 6.9.0.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,8 @@ require 'rmagick'
 root_dir = File.expand_path('..', __dir__)
 IMAGES_DIR = File.join(root_dir, 'doc/ex/images')
 SUPPORT_DIR = File.join(root_dir, 'spec', 'support')
+
+def supported_after(version)
+  magick_lib_version = Magick::Magick_version.split[1].split('-').first
+  :skip if Gem::Version.new(magick_lib_version) < Gem::Version.new(version)
+end
