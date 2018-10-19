6d68595c6c9c48025f89be9765207c473bae1c28
Watson <watson1978@gmail.com>
2018-10-19 11:29:12 +0900

    NormalizeImageChannel() -> SetImageChannelMask() + NormalizeImage()

--- ext/RMagick/rmimage.c
+++ ext/RMagick/rmimage.c
@@ -9449,6 +9449,7 @@ Image_normalize_channel(int argc, VALUE *argv, VALUE self)
 {
     Image *image, *new_image;
     ChannelType channels;
+    ExceptionInfo *exception;
 
     image = rm_check_destroyed(self);
     channels = extract_channels(&argc, argv);
@@ -9460,8 +9461,11 @@ Image_normalize_channel(int argc, VALUE *argv, VALUE self)
 
     new_image = rm_clone_image(image);
 
-    (void) NormalizeImageChannel(new_image, channels);
-    rm_check_image_exception(new_image, DestroyOnError);
+    exception = AcquireExceptionInfo();
+    SetImageChannelMask(new_image, channels);
+    (void) NormalizeImage(new_image, exception);
+    rm_check_exception(exception, new_image, DestroyOnError);
+    (void) DestroyExceptionInfo(exception);
 
     return rm_image_new(new_image);
 }
