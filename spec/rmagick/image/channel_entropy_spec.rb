aa79009bc364b0cc3306d37a2b1c90877c5f2488
Watson <watson1978@gmail.com>
2019-01-20 07:34:39 +0900

    skip channel_entropy spec with unsupported ImageMagick (#311)
    GetImageChannelEntropy() API was implemented in ImageMagick 6.9.0.
    
    If you run channel_entropy spec with unsupported ImageMagick version, this patch will skip it.

--- spec/rmagick/image/channel_entropy_spec.rb
+++ spec/rmagick/image/channel_entropy_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe Magick::Image, '#channel_entropy' do
   let(:img) { Magick::Image.read(IMAGES_DIR + '/Button_0.gif').first }
 
-  it 'returns a channel entropy' do
+  it 'returns a channel entropy', supported_after('6.9.0') do
     res = img.channel_entropy
     puts "res = #{res.inspect}"
     expect(res).to eq([0.5285857222715863])
