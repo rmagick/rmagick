0f1549398178a9d55cc6852ca4cf189dbc9923f6
Watson <watson1978@gmail.com>
2019-02-28 23:18:47 +0900

    Add test for supporting webp format
    Close #60
    
    If ImageMagick was built with webp library, RMagick should handle `webp` format.

--- before_install_linux.sh
+++ before_install_linux.sh
@@ -8,7 +8,7 @@ sudo apt-get autoremove -y imagemagick* libmagick* --purge
 # install build tools, ImageMagick delegates
 sudo apt-get install -y build-essential libx11-dev libxext-dev zlib1g-dev \
   liblcms2-dev libpng-dev libjpeg-dev libfreetype6-dev libxml2-dev \
-  libtiff5-dev vim ghostscript ccache
+  libtiff5-dev libwebp-dev vim ghostscript ccache
 
 if [ -v IMAGEMAGICK_VERSION ]; then
   wget http://www.imagemagick.org/download/releases/ImageMagick-${IMAGEMAGICK_VERSION}.tar.xz
