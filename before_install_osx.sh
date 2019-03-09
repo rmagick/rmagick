3a6fbc08f18d2a6b3e582da1dc612dfe0d8148c3
Robert Fletcher <lobatifricha@gmail.com>
2019-03-08 20:02:41 -0800

    try compiling ImageMagick

--- before_install_osx.sh
+++ before_install_osx.sh
@@ -1,2 +1,21 @@
-brew update
-brew install imagemagick
+#!/usr/bin/env bash
+
+set -euox pipefail
+
+brew install freetype gs
+
+wget http://www.imagemagick.org/download/releases/ImageMagick-${IMAGEMAGICK_VERSION}.tar.xz
+tar -xf ImageMagick-${IMAGEMAGICK_VERSION}.tar.xz
+cd ImageMagick-${IMAGEMAGICK_VERSION}
+
+pwd
+ls
+
+./configure
+make
+sudo make install
+cd ..
+
+gem install bundler
+
+set +u
