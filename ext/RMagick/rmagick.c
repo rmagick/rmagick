74e1a249e050b97895d8fc748e36c2f9cce64a87
Dirk Lemstra <dirk@lemstra.org>
2019-03-25 13:56:37 +0100

    Use methods instead.

--- ext/RMagick/rmagick.c
+++ ext/RMagick/rmagick.c
@@ -149,10 +149,10 @@ MagickInfo_to_format(const MagickInfo *magick_info)
 {
     char mode[4];
 
-    mode[0] = magick_info->blob_support ? '*': ' ';
-    mode[1] = magick_info->decoder ? 'r' : '-';
-    mode[2] = magick_info->encoder ? 'w' : '-';
-    mode[3] = magick_info->encoder && magick_info->adjoin ? '+' : '-';
+    mode[0] = GetMagickBlobSupport(magick_info) ? '*': ' ';
+    mode[1] = GetImageDecoder(magick_info) ? 'r' : '-';
+    mode[2] = GetImageEncoder(magick_info) ? 'w' : '-';
+    mode[3] = GetMagickAdjoin(magick_info) ? '+' : '-';
 
     return rb_str_new(mode, sizeof(mode));
 }
