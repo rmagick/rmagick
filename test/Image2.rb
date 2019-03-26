74237d4f810517c44f7d617e76c028f63d515090
Dirk Lemstra <dirk@lemstra.org>
2019-03-26 17:18:48 +0100

    Removed assert because new version of ImageMagick will not change the image to be PseudoClass.

--- test/Image2.rb
+++ test/Image2.rb
@@ -71,10 +71,10 @@ class Image2_UT < Test::Unit::TestCase
   end
 
   def test_compress_colormap!
-    # DirectClass images are converted to PseudoClass
+    # DirectClass images are converted to PseudoClass in older versions of ImageMagick.
     assert_equal(Magick::DirectClass, @img.class_type)
     assert_nothing_raised { @img.compress_colormap! }
-    assert_equal(Magick::PseudoClass, @img.class_type)
+    # assert_equal(Magick::PseudoClass, @img.class_type)
     @img = Magick::Image.read(IMAGES_DIR + '/Button_0.gif').first
     assert_equal(Magick::PseudoClass, @img.class_type)
     assert_nothing_raised { @img.compress_colormap! }
