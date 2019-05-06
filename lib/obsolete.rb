3285d817cf7427a3622dae4984a74e08f15e45d8
Dirk Lemstra <dirk@lemstra.org>
2019-05-07 00:19:59 +0200

    Deprecated MatteChannel because this will be removed in ImageMagick 7.

--- lib/obsolete.rb
+++ lib/obsolete.rb
@@ -54,6 +54,7 @@ module Magick
   deprecate_constant 'UndefinedMetric'
 
   deprecate_constant 'FlattenAlphaChannel'
+  deprecate_constant 'MatteChannel'
   deprecate_constant 'Rec601LumaColorspace'
   deprecate_constant 'Rec709LumaColorspace'
   deprecate_constant 'ResetAlphaChannel'
