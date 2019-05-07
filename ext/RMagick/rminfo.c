c13a4cd568a87fd0309f8bbe555c28497b6e2e0b
Dirk Lemstra <dirk@lemstra.org>
2019-05-07 21:08:31 +0200

    Removed deprecated Info#group and Info#group=.

--- ext/RMagick/rminfo.c
+++ ext/RMagick/rminfo.c
@@ -1589,53 +1589,6 @@ Info_gravity_eq(VALUE self, VALUE grav)
 
 
 /**
- * Get the group attribute.
- *
- * Ruby usage:
- *   - @verbatim Info#group @endverbatim
- *
- * @param self this object
- * @return the group
- * @deprecated This method has been deprecated.
- */
-VALUE
-Info_group(VALUE self)
-{
-    Info *info;
-
-    rb_warning("Info#group is deprecated");
-    (void) rm_check_destroyed(self);
-    Data_Get_Struct(self, Info, info);
-    return C_long_to_R_long(info->group);
-}
-
-
-/**
- * Set the group attribute.
- *
- * Ruby usage:
- *   - @verbatim Info#group= @endverbatim
- *
- * @param self this object
- * @param value the group
- * @return value
- * @deprecated This method has been deprecated.
- */
-VALUE
-Info_group_eq(VALUE self, VALUE value)
-{
-    Info *info;
-
-    rb_warning("Info#group= is deprecated");
-    (void) rm_check_destroyed(self);
-    rb_check_frozen(self);
-    Data_Get_Struct(self, Info, info);
-    info->group = R_long_to_C_long(value);
-    return value;
-}
-
-
-/**
  * Get the classification type.
  *
  * Ruby usage:
