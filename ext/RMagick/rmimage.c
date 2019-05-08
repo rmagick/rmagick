1adcf078ed4ede495ac00fd0b65257bf9a30da03
Dirk Lemstra <dirk@lemstra.org>
2019-05-08 07:32:19 +0200

    Moved destroy check so it does not need to be skipped in the unit test.

--- ext/RMagick/rmimage.c
+++ ext/RMagick/rmimage.c
@@ -4195,6 +4195,8 @@ Image_morphology_channel(VALUE self, VALUE channel_v, VALUE method_v, VALUE iter
     ChannelType channel;
     KernelInfo *kernel;
 
+    image = rm_check_destroyed(self);
+
     VALUE_TO_ENUM(method_v, method, MorphologyMethod);
     VALUE_TO_ENUM(channel_v, channel, ChannelType);
     Check_Type(iterations, T_FIXNUM);
@@ -4211,7 +4213,6 @@ Image_morphology_channel(VALUE self, VALUE channel_v, VALUE method_v, VALUE iter
 
     Data_Get_Struct(kernel_v, KernelInfo, kernel);
 
-    image = rm_check_destroyed(self);
     exception = AcquireExceptionInfo();
 
     new_image = MorphologyImageChannel(image, channel, method, NUM2LONG(iterations), kernel, exception);
