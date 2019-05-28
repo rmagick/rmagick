4b057237bf7a20e9a808064583718342c65b8279
Dirk Lemstra <dirk@lemstra.org>
2019-05-28 19:39:49 +0200

    Deprecated the use of an unnamed argument for the opacity value of Image#black_threshold and Image#white_threshold. (#709)
    To have a smooth transition from ImageMagick 6 to ImageMagick 7 some of the arguments need to change from an opacity value to an alpha value. Instead of only a Quantum value a named argument should be used. If only a value is passed in the value will be changed from opacity to alpha and a warning will be raised to inform the user that they should use a named argument and switch from opacity to alpha.

--- test/Image1.rb
+++ test/Image1.rb
@@ -322,6 +322,8 @@ class Image1_UT < Test::Unit::TestCase
     assert_nothing_raised { @img.black_threshold(50, 50) }
     assert_nothing_raised { @img.black_threshold(50, 50, 50) }
     assert_nothing_raised { @img.black_threshold(50, 50, 50, 50) }
+    assert_nothing_raised { @img.black_threshold(50, 50, 50, alpha_channel: 50) }
+    assert_raise(ArgumentError) { @img.black_threshold(50, 50, 50, wrong: 50) }
     assert_raise(ArgumentError) { @img.black_threshold(50, 50, 50, 50, 50) }
     res = @img.black_threshold(50)
     assert_instance_of(Magick::Image, res)
