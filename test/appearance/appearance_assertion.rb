34bbc03f504671376c4633771a979761d3806880
Watson <watson1978@gmail.com>
2019-06-01 15:50:46 +0900

    Fix in where retrieve expected image path

--- test/appearance/appearance_assertion.rb
+++ test/appearance/appearance_assertion.rb
@@ -4,7 +4,7 @@ module AppearanceAssertion
   include Test::Unit::Assertions
 
   def assert_same_image(expected_image_path, image_object, delta = 0.01)
-    path = File.expand_path(File.join(__FILE__, '..', expected_image_path))
+    path = File.expand_path(File.join(__dir__, expected_image_path))
 
     expected = Magick::Image.read(path).first
     _, error = expected.compare_channel(image_object, Magick::MeanSquaredErrorMetric)
