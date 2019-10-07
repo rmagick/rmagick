df961564bb9425a0565712e09f17bc6dd011eaab
Robert Fletcher <mockdeep@users.noreply.github.com>
2019-10-07 12:52:55 -0700

    Move test_constitute to RSpec (#818)

--- spec/rmagick/image/constitute_spec.rb
+++ spec/rmagick/image/constitute_spec.rb
@@ -1,13 +1,55 @@
 RSpec.describe Magick::Image, '#constitute' do
   let(:img) { Magick::Image.read(IMAGES_DIR + '/Button_0.gif').first }
-  let(:pixels) { img.dispatch(0, 0, img.columns, img.rows, 'RGBA') }
+  let(:pixels) { img.export_pixels(0, 0, img.columns, img.rows, 'RGBA') }
 
   it 'returns an equivalent image to the given pixels' do
-    res = Magick::Image.constitute(img.columns, img.rows, 'RGBA', pixels)
-    # The constituted image is in MIFF format so we
-    # can't compare it directly to the original image.
-    expect(res.columns).to eq img.columns
-    expect(res.rows).to eq img.rows
-    expect(pixels.all? { |v| v >= 0 && v <= Magick::QuantumRange }).to be true
+    result = Magick::Image.constitute(img.columns, img.rows, 'RGBA', pixels)
+
+    expect(result.export_pixels).to eq(img.export_pixels)
+  end
+
+  it 'allows constituting with RGBA quantum (integer) pixel values' do
+    # [R, G, B, A, R, G, B, A, ...]
+    pixels = [1] * 4 * img.columns * img.rows
+
+    result = Magick::Image.constitute(img.columns, img.rows, 'RGBA', pixels)
+
+    result_pixels = result.dispatch(0, 0, img.columns, img.rows, 'RGBA')
+    expect(result_pixels).to all eq(1)
+  end
+
+  it 'allows constituting with RGBA scale (float) pixel values' do
+    # [R, G, B, A, R, G, B, A, ...]
+    pixels = [1.0] * 4 * img.columns * img.rows
+
+    result = Magick::Image.constitute(img.columns, img.rows, 'RGBA', pixels)
+
+    result_pixels = result.dispatch(0, 0, img.columns, img.rows, 'RGBA')
+    expect(result_pixels).to all eq(65_535)
+  end
+
+  it 'raises an error when invalid RGBA pixel values are given' do
+    pixels = ['x'] * (4 * img.columns * img.rows)
+    expected_message = 'element 0 in pixel array is String, must be numeric'
+
+    expect { Magick::Image.constitute(img.columns, img.rows, 'RGBA', pixels) }
+      .to raise_error(TypeError, expected_message)
+  end
+
+  it 'raises an error when 0 is passed for columns' do
+    expect { Magick::Image.constitute(0, img.rows, 'RGBA', pixels) }
+      .to raise_error(ArgumentError, 'width and height must be non-zero')
+  end
+
+  it 'raises an error when 0 is passed for rows' do
+    expect { Magick::Image.constitute(img.columns, 0, 'RGBA', pixels) }
+      .to raise_error(ArgumentError, 'width and height must be non-zero')
+  end
+
+  it 'raises an error given the wrong number of array elements' do
+    expected_message = 'wrong number of array elements (60960 for 72)'
+
+    expect { Magick::Image.constitute(3, 6, 'RGBA', pixels) }
+      .to raise_error(ArgumentError, expected_message)
   end
 end
