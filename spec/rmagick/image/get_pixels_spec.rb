214685a3fe6cc567710dca6e5c725ef306aed61c
Watson <watson1978@gmail.com>
2019-12-23 19:36:44 +0900

    Add spec of Image#get_pixels

--- spec/rmagick/image/get_pixels_spec.rb
+++ spec/rmagick/image/get_pixels_spec.rb
@@ -13,4 +13,16 @@ RSpec.describe Magick::Image, '#get_pixels' do
     expect { @img.get_pixels(0,  0, @img.columns + 1, 1) }.to raise_error(RangeError)
     expect { @img.get_pixels(0,  0, @img.columns, @img.rows + 1) }.to raise_error(RangeError)
   end
+
+  it 'supports CMYK color' do
+    img = Magick::Image.read(File.join(FIXTURE_PATH, 'cmyk.jpg')).first
+    pixel = img.get_pixels(0, 0, 1, 1).first
+
+    # convert cmyk.jpg -format '%[pixel:p{1,1}]' info:-
+    # => cmyk(49,181,1,183)
+    expect(pixel.cyan).to    equal(49  * 257)
+    expect(pixel.magenta).to equal(181 * 257)
+    expect(pixel.yellow).to  equal(1   * 257)
+    expect(pixel.black).to   equal(183 * 257)
+  end
 end
