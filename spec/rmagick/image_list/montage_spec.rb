67bb745146533a3132eaaef5fbe11208bc5b3de6
Robert Fletcher <lobatifricha@gmail.com>
2020-01-04 13:17:39 -0800

    RSpec: split up montage_spec.rb

--- spec/rmagick/image_list/montage_spec.rb
+++ spec/rmagick/image_list/montage_spec.rb
@@ -3,6 +3,12 @@ RSpec.describe Magick::ImageList, "#montage" do
     @ilist = Magick::ImageList.new
   end
 
+  def assert_same_image(expected_image_path, image_object, delta: 0.01)
+    expected = Magick::Image.read(expected_image_path).first
+    _, error = expected.compare_channel(image_object, Magick::MeanSquaredErrorMetric)
+    expect(error).to be_within(delta).of(0.0)
+  end
+
   it "works" do
     @ilist.read(*Dir[IMAGES_DIR + '/Button_*.gif'])
     ilist = @ilist.copy
@@ -93,5 +99,21 @@ RSpec.describe Magick::ImageList, "#montage" do
       montage = ilist.montage { self.texture = 'x' }
       expect(ilist).to eq(@ilist)
     end.to raise_error(NoMethodError)
+  end
+
+  it 'montages the image' do
+    imagelist = Magick::ImageList.new(IMAGES_DIR + '/Flower_Hat.jpg')
+
+    new_imagelist = imagelist.montage do
+      self.border_width = 100
+      self.border_color = 'red'
+      self.background_color = 'blue'
+      self.matte_color = 'yellow'
+      self.frame = '10x10'
+      self.gravity = Magick::CenterGravity
+    end
+
+    # montage ../../doc/ex/images/Flower_Hat.jpg -border 100x -bordercolor red -mattecolor yellow -background blue -frame 10x10 -gravity Center expected/montage_border_color.jpg
+    assert_same_image(File.join(FIXTURE_PATH, 'montage_border_color.jpg'), new_imagelist.first)
   end
 end
