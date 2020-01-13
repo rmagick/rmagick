a8a384c81359835292fd6562491e1d8db2b07f86
Robert Fletcher <lobatifricha@gmail.com>
2020-01-12 17:36:09 -1000

    RSpec: remove extra spec setup
    When I split up the tests I copied the `before` blocks to each spec.
    Some of the logic in the `before` block didn't apply to most specs,
    though, so this removes the extra logic from the specs that don't need
    them.

--- spec/rmagick/image/image_type_spec.rb
+++ spec/rmagick/image/image_type_spec.rb
@@ -1,14 +1,6 @@
 RSpec.describe Magick::Image, '#image_type' do
   before do
     @img = described_class.new(100, 100)
-    gc = Magick::Draw.new
-
-    gc.stroke_width(5)
-    gc.circle(50, 50, 80, 80)
-    gc.draw(@img)
-
-    @hat = described_class.read(FLOWER_HAT).first
-    @p = described_class.read(IMAGE_WITH_PROFILE).first.color_profile
   end
 
   it 'works' do
