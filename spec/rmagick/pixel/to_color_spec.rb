e9a6366c00a3c572b834807739856f2b057e205c
Watson <watson1978@gmail.com>
2020-01-13 21:01:44 +0900

    Handle alpha value in Pixel#to_color (#922)
    Fix #921

--- spec/rmagick/pixel/to_color_spec.rb
+++ spec/rmagick/pixel/to_color_spec.rb
@@ -22,4 +22,10 @@ RSpec.describe Magick::Pixel, '#to_color' do
     expect { @pixel.to_color(Magick::AllCompliance, false, 32) }.to raise_error(ArgumentError)
     expect { @pixel.to_color(1) }.to raise_error(TypeError)
   end
+
+  it 'return alpha value if pixel has alpha' do
+    @pixel.alpha = 123
+    expect(@pixel.to_color(Magick::AllCompliance, true)).to eq('#A5A52A2A2A2A007B')
+    expect(@pixel.to_color(Magick::AllCompliance, false)).to eq('brown')
+  end
 end
