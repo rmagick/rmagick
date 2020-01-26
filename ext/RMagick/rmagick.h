0dac4ee3baa9231a21a48e6d8339c8aa09f31cc0
Watson <watson1978@gmail.com>
2020-01-26 22:24:00 +0900

    image

--- ext/RMagick/rmagick.h
+++ ext/RMagick/rmagick.h
@@ -532,9 +532,10 @@ EXTERN ID rm_ID_y;                 /**< "y" */
    } while(0)
 
 extern const rb_data_type_t rm_draw_data_type;
+extern const rb_data_type_t rm_image_data_type;
 
 #define GetDrawStruct(obj, var) TypedData_Get_Struct(obj, Draw, &rm_draw_data_type, var);
-#define GetImageStruct(obj, var) Data_Get_Struct(obj, Image, var);
+#define GetImageStruct(obj, var) TypedData_Get_Struct(obj, Image, &rm_image_data_type, var);
 #define GetMagickEnumStruct(obj, var) Data_Get_Struct(obj, MagickEnum, var);
 #define GetInfoStruct(obj, var) Data_Get_Struct(obj, Info, var);
 #define GetMontageStruct(obj, var) Data_Get_Struct(obj, Montage, var);
