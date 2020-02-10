73270458e770aac3600c1b17d6c6d70c77b84412
Robert Fletcher <mockdeep@users.noreply.github.com>
2020-02-10 09:46:51 -0800

    RSpec: remove @list2 from specs where it is unused (#1017)
    Again, when splitting up the tests, these got copied across all of the
    specs, but it's only used in a handful of them. I left it in the tests
    where it is used.

--- spec/rmagick/image_list/find_spec.rb
+++ spec/rmagick/image_list/find_spec.rb
@@ -1,12 +1,6 @@
 RSpec.describe Magick::ImageList, '#find' do
   before do
     @list = described_class.new(*FILES[0..9])
-    @list2 = described_class.new # intersection is 5..9
-    @list2 << @list[5]
-    @list2 << @list[6]
-    @list2 << @list[7]
-    @list2 << @list[8]
-    @list2 << @list[9]
   end
 
   it 'works' do
