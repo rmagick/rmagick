50ddcd83b23dd440f9a0bc47b3b6238fc9e0888c
Robert Fletcher <mockdeep@users.noreply.github.com>
2020-02-16 15:15:45 -0800

    Rubocop: enable HeredocArgumentClosingParenthesis (#1059)
    This rule requires that the closing paren is after the opening heredoc
    tag.
    
    https://rubocop.readthedocs.io/en/stable/cops_layout/#layoutheredocargumentclosingparenthesis

--- doc/ex/arcs01.rb
+++ doc/ex/arcs01.rb
@@ -15,14 +15,14 @@ rvg = Magick::RVG.new(12.cm, 5.25.cm).viewbox(0, 0, 1200, 400) do |canvas|
         .styles(fill: 'red', stroke: 'blue', stroke_width: 5)
   canvas.path('M275,175 v-150 a150,150 0 0,0 -150,150 z')
         .styles(fill: 'yellow', stroke: 'blue', stroke_width: 5)
-  canvas.path(<<-END_PATH
+  canvas.path(<<-END_PATH)
            M600,350 l 50,-25
            a25,25 -30 0,1 50,-25 l 50,-25
            a25,50 -30 0,1 50,-25 l 50,-25
            a25,75 -30 0,1 50,-25 l 50,-25
            a25,100 -30 0,1 50,-25 l 50,-25
   END_PATH
-             ).styles(fill: 'none', stroke: 'red', stroke_width: 5)
+        .styles(fill: 'none', stroke: 'red', stroke_width: 5)
 end
 
 rvg.draw.write('arcs01.gif')
