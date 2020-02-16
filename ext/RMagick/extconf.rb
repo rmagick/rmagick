68cd092727b0bdbd9f2288cb0dd6a039c03fdbe5
Robert Fletcher <mockdeep@users.noreply.github.com>
2020-02-16 11:23:49 -0800

    IM: raise when no ImageMagick executable is found (#1015)

--- ext/RMagick/extconf.rb
+++ ext/RMagick/extconf.rb
@@ -375,7 +375,13 @@ module RMagick
     end
 
     def magick_command
-      @magick_command ||= find_executable('magick') ? 'magick' : 'identify'
+      @magick_command ||= if find_executable('magick')
+                            'magick'
+                          elsif find_executable('identify')
+                            'identify'
+                          else
+                            raise NotImplementedError, "no executable found for ImageMagick"
+                          end
     end
 
     def im_version_at_least?(version)
