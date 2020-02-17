f0929e82dea2792f7c42809d9269b06407bfcabf
Watson <watson1978@gmail.com>
2020-02-18 01:56:05 +0900

    Add spec for PDF problem

--- spec/rmagick/image/read_spec.rb
+++ spec/rmagick/image/read_spec.rb
@@ -25,4 +25,10 @@ RSpec.describe Magick::Image, '#read' do
       expect { described_class.read(nil) }.to raise_error(Magick::ImageMagickError, /unable to open image nil/)
     end
   end
+
+  describe 'issue #483' do
+    it 'can read PDF file' do
+      expect { described_class.read(File.join(FIXTURE_PATH, 'sample.pdf')) }.not_to raise_error
+    end
+  end
 end
