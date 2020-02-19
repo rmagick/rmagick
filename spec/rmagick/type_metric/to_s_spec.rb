b65b7f133cdb1765b6ac361d603e8f41b4684169
Robert Fletcher <lobatifricha@gmail.com>
2020-02-18 19:43:19 -0800

    RSpec: check for exact type metric value

--- spec/rmagick/type_metric/to_s_spec.rb
+++ spec/rmagick/type_metric/to_s_spec.rb
@@ -2,6 +2,6 @@ describe Magick::TypeMetric, '#to_s' do
   it 'works' do
     draw = Magick::Draw.new
     metric = draw.get_type_metrics('ABCDEF')
-    expect(metric.to_s).to match(/^pixels_per_em=\(x=.+,y=.+\) ascent=.+ descent=.+ width=.+ height=.+ max_advance=.+ bounds.x1=.+ bounds.y1=.+ bounds.x2=.+ bounds.y2=.+ underline_position=.+ underline_thickness=.+$/)
+    expect(metric.to_s).to eq("pixels_per_em=(x=12,y=12) ascent=9 descent=-3 width=49.7969 height=15 max_advance=13 bounds.x1=0.203125 bounds.y1=0 bounds.x2=8.125 bounds.y2=9 underline_position=-2.35938 underline_thickness=-2.35938")
   end
 end
