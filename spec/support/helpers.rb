b4d4650a04b33cee0f3eb0b17ca5844a8d963e6e
Leigh Arber <leigharber@gmail.com>
2020-04-05 22:33:36 -0500

    RSpec: Flesh out channel_spec
    Extracted helpers from charcoal_spec to use in channel_spec

---
+++ spec/support/helpers.rb
@@ -0,0 +1,16 @@
+module TestHelpers
+  TEST_PIXELS = {
+    RGB: [[45, 98, 156], [209, 171, 11], [239, 236, 2], [8, 65, 247]],
+    CMYK: [[23, 54, 186, 76], [29, 71, 131, 122], [239, 206, 99, 76], [84, 165, 47, 54]]
+  }
+
+  def build_image(mode: "RGB")
+    pixels = TEST_PIXELS.fetch(mode.to_sym)
+    image = Magick::Image.new(2, 2)
+    image.import_pixels(0, 0, 2, 2, mode, pixels.flatten)
+  end
+
+  def gray(pixel_value)
+    [pixel_value, pixel_value, pixel_value]
+  end
+end
