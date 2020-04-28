86e17c972d4fa845a92059b5cdf8a0128d5946ad
Watson <watson1978@gmail.com>
2020-04-28 23:20:57 +0900

    RSpec: Add specs to confirm that ImageList#remap accepts ImageList object as argument

--- spec/rmagick/image_list/remap_spec.rb
+++ spec/rmagick/image_list/remap_spec.rb
@@ -15,4 +15,14 @@ RSpec.describe Magick::ImageList, "#remap" do
     expect { image_list.remap(remap_image) }.to raise_error(Magick::DestroyedImageError)
     # expect { image_list.affinity(affinity_image, 1) }.to raise_error(TypeError)
   end
+
+  it 'accepts an ImageList argument' do
+    image_list = described_class.new
+
+    image_list.read(*Dir[IMAGES_DIR + '/Button_*.gif'])
+    remap_image_list = described_class.new
+    remap_image_list.new_image(10, 10)
+    expect { image_list.remap(remap_image_list) }.not_to raise_error
+    expect { image_list.remap(remap_image_list, Magick::NoDitherMethod) }.not_to raise_error
+  end
 end
