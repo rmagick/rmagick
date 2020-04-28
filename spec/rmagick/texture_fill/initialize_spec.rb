03b6e312aaebd7ffa80b3acf66371d9e69814476
Watson <watson1978@gmail.com>
2020-04-28 23:17:56 +0900

    RSpec: Add specs to confirm that Magick::TextureFill.new accepts ImageList object as argument

--- spec/rmagick/texture_fill/initialize_spec.rb
+++ spec/rmagick/texture_fill/initialize_spec.rb
@@ -3,4 +3,10 @@ RSpec.describe Magick::TextureFill, '#initialize' do
     granite = Magick::Image.read('granite:').first
     expect(described_class.new(granite)).to be_instance_of(described_class)
   end
+
+  it 'accepts an ImageList argument' do
+    image_list = Magick::ImageList.new
+    image_list.new_image(10, 10)
+    expect(described_class.new(image_list)).to be_instance_of(described_class)
+  end
 end
