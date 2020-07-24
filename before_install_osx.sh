558aa0063234479a84440e677158f3afdd312f40
Watson <watson1978@gmail.com>
2020-07-24 09:14:05 +0900

    CI: Fix typo in osx setup script (#1227)

--- before_install_osx.sh
+++ before_install_osx.sh
@@ -18,7 +18,7 @@ fi
 export HOMEBREW_NO_AUTO_UPDATE=true
 brew install wget pkg-config ghostscript freetype jpeg little-cms2 libomp libpng libtiff liblqr libtool libxml2 zlib webp
 
-export LDFLAGS="-L/usr/local/opt/libxml2/lib -L/usr/local/opt/zlib/li"
+export LDFLAGS="-L/usr/local/opt/libxml2/lib -L/usr/local/opt/zlib/lib"
 export CPPFLAGS="-I/usr/local/opt/libxml2/include/libxml2 -I/usr/local/opt/zlib/include"
 
 project_dir=$(pwd)
