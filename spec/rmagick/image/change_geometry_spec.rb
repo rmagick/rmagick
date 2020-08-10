51ed16ab519a97349bdb2fc994a23c2400f2181e
Le1gh <Leigharber@gmail.com>
2020-08-09 22:16:41 -0500

    Flesh out change_geometry_spec. (#1230)

--- spec/rmagick/image/change_geometry_spec.rb
+++ spec/rmagick/image/change_geometry_spec.rb
@@ -1,13 +1,71 @@
 RSpec.describe Magick::Image, "#change_geometry" do
-  it "works" do
-    image = described_class.new(20, 20)
+  it "passes the original image to the block" do
+    image = build_image
+    block_image = nil
+    image.change_geometry('400x400') do |_cols, _rows, inner_image|
+      block_image = inner_image
+    end
 
-    expect { image.change_geometry('sss') }.to raise_error(ArgumentError)
-    expect { image.change_geometry('100x100') }.to raise_error(LocalJumpError)
+    expect(block_image).to be(image)
+  end
 
-    result = image.change_geometry('100x100') { 1 }
-    expect(result).to eq(1)
+  it "returns the result from the block" do
+    image = build_image
 
-    expect { image.change_geometry([]) }.to raise_error(ArgumentError)
+    result = image.change_geometry('400x400') { "some_result" }
+
+    expect(result).to eq("some_result")
+  end
+
+  it "does not directly modify the original image" do
+    image = build_image
+
+    image.change_geometry('400x400') {}
+
+    expect([image.rows, image.columns]).to eq([2, 2])
+  end
+
+  it "passes dimensions to enable enlarging the image" do
+    image = build_image
+
+    resized_image = image.change_geometry('400x400') do |cols, rows, block_image|
+      block_image.resize(cols, rows)
+    end
+
+    expect([resized_image.rows, resized_image.columns]).to eq([400, 400])
+  end
+
+  it "passes dimensions that will maintain the aspect ratio" do
+    image = build_image
+
+    image.change_geometry('300x4000') do |cols, rows|
+      image.resize!(cols, rows)
+    end
+
+    expect([image.rows, image.columns]).to eq([300, 300])
+  end
+
+  it "accepts a Geometry object that maintains the aspect ratio" do
+    image = build_image
+
+    image.change_geometry(Magick::Geometry.new(300, 4000)) do |cols, rows|
+      image.resize!(cols, rows)
+    end
+
+    expect([image.rows, image.columns]).to eq([300, 300])
+  end
+
+  it "raises an error when extra arguments are passed" do
+    image = build_image
+
+    expect { image.change_geometry('400x400', "boo") {} }
+      .to raise_error(ArgumentError)
+  end
+
+  it "raises an error when no block is passed" do
+    image = build_image
+
+    expect { image.change_geometry('400x400') }
+      .to raise_error(LocalJumpError)
   end
 end
