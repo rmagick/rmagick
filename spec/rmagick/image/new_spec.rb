65e666aa29bfff94f0922a449f8b2cab8e1fefb1
akicho8 <akicho8@gmail.com>
2021-03-12 15:00:57 +0900

    Refactor: Warning test is removed from new_spec.rb

--- spec/rmagick/image/new_spec.rb
+++ spec/rmagick/image/new_spec.rb
@@ -3,24 +3,12 @@ RSpec.describe Magick::Image, '#new' do
     self_obj = nil
     yield_obj = nil
 
-    # call instance_eval
-    described_class.new(20, 20) do
-      self_obj = self
-    end
-    expect(self_obj).to be_instance_of(Magick::Image::Info)
-
-    # call yield
     described_class.new(20, 20) do |e|
       yield_obj = e
       self_obj = self
     end
+
     expect(yield_obj).to be_instance_of(Magick::Image::Info)
     expect(self_obj).to eq(self)
-
-    # Able to write in the following manner by calling in yield
-    #
-    # @background_color = 'red'
-    # image = described_class.new(20, 20) { |e| e.background_color = @background_color }
-    # expect(image.background_color).to eq('red')
   end
 end
