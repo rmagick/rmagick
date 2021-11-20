a9e0ccb170270a6b96ab34b8c8cdc9603d98dcff
Dirk Lemstra <dirk@lemstra.org>
2021-11-20 17:29:40 +0100

    Skip unsupported kernels instead.

--- spec/rmagick/kernel_info/initialize_spec.rb
+++ spec/rmagick/kernel_info/initialize_spec.rb
@@ -3,9 +3,7 @@ RSpec.describe Magick::KernelInfo, '#initialize' do
     Magick::KernelInfoType.values do |kernel|
       k = kernel.to_s.sub('Kernel', '')
 
-      if [Magick::UserDefinedKernel, Magick::UndefinedKernel].include?(kernel)
-        expect { described_class.new(k) }.to raise_error(RuntimeError)
-      else
+      if ![Magick::UserDefinedKernel, Magick::UndefinedKernel].include?(kernel)
         expect(described_class.new(k)).to be_instance_of(described_class)
       end
     end
