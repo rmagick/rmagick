6a21e278de9ad7e58df9664dce4d2f52df4e800b
Dirk Lemstra <dirk@lemstra.org>
2021-11-20 17:37:31 +0100

    Another style change.

--- spec/rmagick/kernel_info/initialize_spec.rb
+++ spec/rmagick/kernel_info/initialize_spec.rb
@@ -3,9 +3,7 @@ RSpec.describe Magick::KernelInfo, '#initialize' do
     Magick::KernelInfoType.values do |kernel|
       k = kernel.to_s.sub('Kernel', '')
 
-      unless [Magick::UserDefinedKernel, Magick::UndefinedKernel].include?(kernel)
-        expect(described_class.new(k)).to be_instance_of(described_class)
-      end
+      expect(described_class.new(k)).to be_instance_of(described_class) unless [Magick::UserDefinedKernel, Magick::UndefinedKernel].include?(kernel)
     end
     expect { described_class.new('') }.to raise_error(RuntimeError)
     expect { described_class.new(42) }.to raise_error(TypeError)
