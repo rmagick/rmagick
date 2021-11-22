037a0ec79fa0a94778dabb6ee1645c01bdeb5298
Dirk Lemstra <dirk@lemstra.org>
2021-11-22 19:46:54 +0100

    Use next if instead.

--- spec/rmagick/kernel_info/initialize_spec.rb
+++ spec/rmagick/kernel_info/initialize_spec.rb
@@ -1,9 +1,11 @@
 RSpec.describe Magick::KernelInfo, '#initialize' do
   it 'works' do
     Magick::KernelInfoType.values do |kernel|
+      next if [Magick::UserDefinedKernel, Magick::UndefinedKernel].include?(kernel)
+
       k = kernel.to_s.sub('Kernel', '')
 
-      expect(described_class.new(k)).to be_instance_of(described_class) unless [Magick::UserDefinedKernel, Magick::UndefinedKernel].include?(kernel)
+      expect(described_class.new(k)).to be_instance_of(described_class)
     end
     expect { described_class.new('') }.to raise_error(RuntimeError)
     expect { described_class.new(42) }.to raise_error(TypeError)
