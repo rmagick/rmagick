a82bfc33949b1e3f9d46cce1cd685d74aa02a76a
Dirk Lemstra <dirk@lemstra.org>
2021-11-22 20:29:58 +0100

    Magick::UndefinedKernel should also not be used when creating a new KernelInfo. (#1312)

--- spec/rmagick/kernel_info/initialize_spec.rb
+++ spec/rmagick/kernel_info/initialize_spec.rb
@@ -1,13 +1,11 @@
 RSpec.describe Magick::KernelInfo, '#initialize' do
   it 'works' do
     Magick::KernelInfoType.values do |kernel|
+      next if [Magick::UserDefinedKernel, Magick::UndefinedKernel].include?(kernel)
+
       k = kernel.to_s.sub('Kernel', '')
 
-      if kernel == Magick::UserDefinedKernel
-        expect { described_class.new(k) }.to raise_error(RuntimeError)
-      else
-        expect(described_class.new(k)).to be_instance_of(described_class)
-      end
+      expect(described_class.new(k)).to be_instance_of(described_class)
     end
     expect { described_class.new('') }.to raise_error(RuntimeError)
     expect { described_class.new(42) }.to raise_error(TypeError)
