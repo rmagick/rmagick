c76842e6e48f521b06b1add06cd855934f722a7b
Watson <watson1978@gmail.com>
2023-04-21 00:38:18 +0900

    CI: Add actions/cache@v3 (#1385)

--- before_install_linux.sh
+++ before_install_linux.sh
@@ -24,7 +24,7 @@ sudo apt-get autoremove -y imagemagick* libmagick* --purge
 # install build tools, ImageMagick delegates
 sudo apt-get install -y build-essential libx11-dev libxext-dev zlib1g-dev \
   liblcms2-dev libpng-dev libjpeg-dev libfreetype6-dev libxml2-dev \
-  libtiff5-dev libwebp-dev liblqr-1-0-dev vim gsfonts ghostscript ccache
+  libtiff5-dev libwebp-dev liblqr-1-0-dev vim gsfonts ghostscript
 
 if [ ! -d /usr/include/freetype ]; then
   # If `/usr/include/freetype` is not existed, ImageMagick 6.7 configuration fails about Freetype.
@@ -52,7 +52,7 @@ build_imagemagick() {
   fi
 
   cd "${build_dir}"
-  CC="ccache cc" CXX="ccache c++" ./configure --prefix=/usr "${options}"
+  ./configure --prefix=/usr "${options}"
   make -j
 }
 
