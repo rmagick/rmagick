60b2b1e83b4bf73670ccd7ab9421c3d3235c3ec4
Watson <watson1978@gmail.com>
2023-07-08 18:16:35 +0900

    Doc: Reformat the HTML and CSS

--- doc/scripts/stripeTables.js
+++ doc/scripts/stripeTables.js
@@ -1,23 +1,21 @@
 // Alternate gray/white rows in all the tables having the class "striped"
 function stripeTables() {
-    if (!document.getElementsByTagName) {
-        return false;
-    }
-    var tables = document.getElementsByTagName("table");
-    for (var i = 0; i < tables.length; i++) {
-        if (tables[i].className == "striped") {
-            var gray = true;
-            var rows = tables[i].getElementsByTagName("tr");
-            for (var j = 0; j < rows.length; j++) {
-                if (gray) {
-                    rows[j].style.backgroundColor = "#f0f0f0";
-                    gray = false;
-                }
-                else {
-                    gray = true;
-                }
-            }
+  if (!document.getElementsByTagName) {
+    return false;
+  }
+  var tables = document.getElementsByTagName("table");
+  for (var i = 0; i < tables.length; i++) {
+    if (tables[i].className == "striped") {
+      var gray = true;
+      var rows = tables[i].getElementsByTagName("tr");
+      for (var j = 0; j < rows.length; j++) {
+        if (gray) {
+          rows[j].style.backgroundColor = "#f0f0f0";
+          gray = false;
+        } else {
+          gray = true;
         }
+      }
     }
+  }
 }
-
