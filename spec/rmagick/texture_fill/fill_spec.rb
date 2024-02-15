4bdfaebf610861dd1152c4c93f3b4e1c3b128ae9
Watson <watson1978@gmail.com>
2024-02-16 01:54:43 +0900

    Fix Magick::GradientFill#fill and Magick::TextureFill#fill to accept ImageList object
    Magick::HatchFill#fill accepts Magick::ImageList object.
    ```
    irb(main):001:0> require 'rmagick'
    => true
    irb(main):002:0> imgl = Magick::ImageList.new
    =>
    []
    ...
    irb(main):003:0> imgl.new_image(10, 10)
    =>
    [  10x10 DirectClass 16-bit]
    scene=0
    irb(main):004:0> gradient = Magick::HatchFill.new('white', 'lightcyan2')
    irb(main):005:0> gradient.fill(imgl)
    => 10
    ```
    
    However, Magick::GradientFill#fill does not accept it.
    ```
    irb(main):001:0> require 'rmagick'
    => true
    irb(main):002:0> imgl = Magick::ImageList.new
    =>
    []
    ...
    irb(main):003:0> imgl.new_image(10, 10)
    =>
    [  10x10 DirectClass 16-bit]
    scene=0
    irb(main):004:0> gradient = Magick::GradientFill.new(0, 0, 0, 0, '#900', '#000')
    irb(main):005:0> gradient.fill(imgl)
    (irb):5:in `fill': wrong argument type Magick::ImageList (expected Magick::Image) (TypeError)
    ```
    
    Fix inconsistent behaviour.

--- spec/rmagick/texture_fill/fill_spec.rb
+++ spec/rmagick/texture_fill/fill_spec.rb
@@ -6,5 +6,11 @@ RSpec.describe Magick::TextureFill, '#fill' do
     image = Magick::Image.new(10, 10)
     obj = texture.fill(image)
     expect(obj).to eq(texture)
+
+    imgl = Magick::ImageList.new
+    imgl.new_image(10, 10)
+
+    obj = texture.fill(imgl)
+    expect(obj).to eq(texture)
   end
 end
