c16a814fa341225b18a80f63b0b8b374368e02b0
Watson <watson1978@gmail.com>
2024-02-18 02:05:22 +0900

    Doc: Add Magick::SolidFill document

---
+++ doc/ex/solidfill.rb
@@ -0,0 +1,20 @@
+require 'rmagick'
+
+# Demonstrate the SolidFill class
+
+fill = Magick::SolidFill.new('orange')
+img = Magick::Image.new(Cols, Rows, fill)
+
+# Annotate the filled image with the code that created the fill.
+
+ann = Magick::Draw.new
+ann.annotate(img, 0, 0, 0, 0, "SolidFill.new('orange')") do |options|
+  options.gravity = Magick::CenterGravity
+  options.fill = 'white'
+  options.stroke = 'transparent'
+  options.pointsize = 14
+end
+
+# img.display
+img.write('solidfill.gif')
+exit
