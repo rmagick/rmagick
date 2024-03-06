0b4974347e46bc419f06c2d54c9eff7cf4c0d732
Watson <watson1978@gmail.com>
2024-03-07 02:17:39 +0900

    Fix Draw#{stroke_dasharray, stroke_miterlimit} to accept object which has #to_f method (#1486)
    The Draw method implemented in magick_internal.rb can implicitly convert objects with #to_f.

--- spec/rmagick/draw/stroke_miterlimit_spec.rb
+++ spec/rmagick/draw/stroke_miterlimit_spec.rb
@@ -9,5 +9,6 @@ RSpec.describe Magick::Draw, '#stroke_miterlimit' do
 
     expect { draw.stroke_miterlimit(0.9) }.to raise_error(ArgumentError)
     expect { draw.stroke_miterlimit('foo') }.to raise_error(ArgumentError)
+    expect { draw.stroke_miterlimit('1.0') }.not_to raise_error
   end
 end
