fd9e8b6e0a9aee740ecf496e96c26ce1626f9d79
Watson <watson1978@gmail.com>
2024-03-06 16:38:18 +0900

    Fix Image#composite_affine to accept ImageList object
    Similar with https://github.com/rmagick/rmagick/pull/1467

--- spec/rmagick/image/composite_affine_spec.rb
+++ spec/rmagick/image/composite_affine_spec.rb
@@ -10,4 +10,13 @@ RSpec.describe Magick::Image, '#composite_affine' do
     expect(result).to be_instance_of(described_class)
     expect(result).not_to be(image1)
   end
+
+  it 'accepts an ImageList argument' do
+    affine = Magick::AffineMatrix.new(1, 0, 1, 0, 0, 0)
+    image = described_class.new(20, 20)
+    image_list = Magick::ImageList.new
+    image_list.new_image(20, 20)
+
+    expect { image.composite_affine(image_list, affine) }.not_to raise_error
+  end
 end
