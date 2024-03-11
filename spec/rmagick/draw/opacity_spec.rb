2db04366b805152602f8fd2d7db160a0e77ba536
Watson <watson1978@gmail.com>
2024-03-12 06:22:13 +0900

    Fix Draw#{fill_opacity, opacity, stroke_opacity} to correctly handle arguments (#1492)
    Similar with https://github.com/rmagick/rmagick/pull/1491

--- spec/rmagick/draw/opacity_spec.rb
+++ spec/rmagick/draw/opacity_spec.rb
@@ -7,6 +7,11 @@ RSpec.describe Magick::Draw, '#opacity' do
     expect(draw.inspect).to eq('opacity 0.8')
     expect { draw.draw(image) }.not_to raise_error
 
+    draw = described_class.new
+    draw.opacity(1/4r)
+    expect(draw.inspect).to eq('opacity 0.25')
+    expect { draw.draw(image) }.not_to raise_error
+
     expect { draw.opacity(0.0) }.not_to raise_error
     expect { draw.opacity(1.0) }.not_to raise_error
     expect { draw.opacity('0.0') }.not_to raise_error
@@ -18,5 +23,6 @@ RSpec.describe Magick::Draw, '#opacity' do
     expect { draw.opacity('-0.01') }.to raise_error(ArgumentError)
     expect { draw.opacity('1.01') }.to raise_error(ArgumentError)
     expect { draw.opacity('xxx') }.to raise_error(ArgumentError)
+    expect { draw.opacity('%20') }.to raise_error(ArgumentError)
   end
 end
