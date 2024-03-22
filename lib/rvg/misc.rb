c9e3ad0a94df2584564f860ed6fcc6df7c977ea5
Watson <watson1978@gmail.com>
2024-03-22 15:22:47 +0900

    Fix GraphicContext#font_weight to accept Numeric object
    Related to https://github.com/rmagick/rmagick/pull/1509

--- lib/rvg/misc.rb
+++ lib/rvg/misc.rb
@@ -579,7 +579,7 @@ module Magick
 
         def font_weight(weight)
           # If the arg is not in the hash use it directly. Handles numeric values.
-          weight = FONT_WEIGHT.fetch(weight.to_sym) { |key| key }
+          weight = weight.is_a?(Numeric) ? weight : FONT_WEIGHT.fetch(weight.to_sym, Magick::NormalWeight)
           @gc.font_weight(weight)
           @shadow[-1].font_weight = weight
           nil
