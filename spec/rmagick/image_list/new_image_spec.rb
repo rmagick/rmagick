cae2373281b8d2fa9f53247b3198d338846c5e4c
Watson <watson1978@gmail.com>
2024-03-24 07:18:02 +0900

    Improve HatchFill to accept Pixel object as color

--- spec/rmagick/image_list/new_image_spec.rb
+++ spec/rmagick/image_list/new_image_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Magick::ImageList, "#new_image" do
 
     expect(image_list.length).to eq(1)
     expect(image_list.scene).to eq(0)
-    image_list.new_image(20, 20, Magick::HatchFill.new('black'))
+    image_list.new_image(20, 20, Magick::HatchFill.new(Magick::Pixel.from_color('black'), Magick::Pixel.from_color('white')))
     expect(image_list.length).to eq(2)
     expect(image_list.scene).to eq(1)
     image_list.new_image(20, 20) { |options| options.background_color = 'red' }
