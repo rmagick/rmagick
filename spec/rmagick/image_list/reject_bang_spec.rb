0268cdeefaf158162e32f5ef1766671566b17f6a
Watson <watson1978@gmail.com>
2024-04-18 14:40:09 +0900

    Rubocop: Fix Performance/StringInclude

--- spec/rmagick/image_list/reject_bang_spec.rb
+++ spec/rmagick/image_list/reject_bang_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe Magick::ImageList, '#reject!' do
     image_list.scene = 7
     cur = image_list.cur_image
 
-    image_list.reject! { |image| File.basename(image.filename) =~ /5/ }
+    image_list.reject! { |image| File.basename(image.filename).include?('5') }
     expect(image_list).to be_instance_of(described_class)
     expect(image_list.length).to eq(9)
     expect(image_list.cur_image).to be(cur)
 
     # Delete the current image
-    image_list.reject! { |image| File.basename(image.filename) =~ /7/ }
+    image_list.reject! { |image| File.basename(image.filename).include?('7') }
     expect(image_list).to be_instance_of(described_class)
     expect(image_list.length).to eq(8)
     expect(image_list.cur_image).to be(image_list[-1])
