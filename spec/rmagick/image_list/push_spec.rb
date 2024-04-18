68225e1fc210643224156562a8fa057bce4b5d9f
Watson <watson1978@gmail.com>
2024-04-19 04:52:17 +0900

    Rubocop: Fix Lint/SelfAssignment (#1573)

--- spec/rmagick/image_list/push_spec.rb
+++ spec/rmagick/image_list/push_spec.rb
@@ -2,7 +2,6 @@ RSpec.describe Magick::ImageList, '#push' do
   it 'works' do
     image_list = described_class.new(*FILES[0..9])
 
-    image_list = image_list
     image1 = image_list[0]
     image2 = image_list[1]
     image_list2 = nil
