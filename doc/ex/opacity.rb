c89afe0ef852e3ab4c775a385329beac23c97d90
Watson <watson1978@gmail.com>
2024-05-09 18:57:31 +0900

    Doc: Use fill_opacity instead of opacity

--- doc/ex/opacity.rb
+++ doc/ex/opacity.rb
@@ -9,16 +9,16 @@ gc.stroke('blue')
 gc.fill('yellow')
 gc.stroke_width(10)
 
-gc.opacity('25%')
+gc.fill_opacity('25%')
 gc.roundrectangle(20, 20, 60, 90, 5, 5)
 
-gc.opacity('50%')
+gc.fill_opacity('50%')
 gc.roundrectangle(80, 20, 120, 90, 5, 5)
 
-gc.opacity(0.75)
+gc.fill_opacity(0.75)
 gc.roundrectangle(140, 20, 180, 90, 5, 5)
 
-gc.opacity(1)
+gc.fill_opacity(1)
 gc.roundrectangle(200, 20, 240, 90, 5, 5)
 
 gc.font_weight(Magick::NormalWeight)
