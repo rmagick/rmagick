3fe553888a990b615c680c4dab5d56963baaccbf
Shizuo Fujita <watson1978@gmail.com>
2026-03-07 12:59:39 +0900

    Rubocop: Fix Style/ClassEqualityComparison (#1735)

--- spec/rmagick/image_list/all_predicate_spec.rb
+++ spec/rmagick/image_list/all_predicate_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Magick::ImageList, '#all?' do
     image_list = described_class.new(*FILES[0..9])
 
     q = nil
-    expect { q = image_list.all? { |i| i.class == Magick::Image } }.not_to raise_error
+    expect { q = image_list.all?(Magick::Image) }.not_to raise_error
     expect(q).to be(true)
   end
 end
