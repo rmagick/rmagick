7c778e6497f1f199cdcb835059d36241c5d39948
Shizuo Fujita <fujita@clear-code.com>
2026-04-05 17:23:09 +0900

    spec: Remove unused constants

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,15 +10,10 @@ require 'rvg/rvg'
 
 root_dir = File.expand_path('..', __dir__)
 IMAGES_DIR = File.join(root_dir, 'doc/ex/images')
-SUPPORT_DIR = File.join(__dir__, 'support')
 FIXTURE_PATH = File.join(__dir__, 'fixtures')
 FILES = Dir[IMAGES_DIR + '/Button_*.gif']
 FLOWER_HAT = IMAGES_DIR + '/Flower_Hat.jpg'
 IMAGE_WITH_PROFILE = IMAGES_DIR + '/image_with_profile.jpg'
-
-Magick::Magick_version =~ /ImageMagick (\d+\.\d+\.\d+)-(\d+) /
-abort 'Unable to get ImageMagick version' unless Regexp.last_match(1) && Regexp.last_match(2)
-IM_VERSION = Gem::Version.new(Regexp.last_match(1))
 
 RSpec.configure do |config|
   config.include(TestHelpers)
