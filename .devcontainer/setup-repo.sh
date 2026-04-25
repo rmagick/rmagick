b1d3df7e527b47322777d9980b4b957c25bbfce2
Shizuo Fujita <fujita@clear-code.com>
2026-04-25 10:35:32 +0900

    Dev: Remove unnecessary bundle path

--- .devcontainer/setup-repo.sh
+++ .devcontainer/setup-repo.sh
@@ -7,4 +7,4 @@ apt-get install -y sudo
 bash /workspaces/rmagick/before_install_linux.sh
 
 cd /workspaces/rmagick
-bundle install --path=vendor/bundle --jobs 4 --retry 3
+bundle install --jobs 4 --retry 3
